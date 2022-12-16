dd64fb45212d3bb098ef08849b0ecaf5d25843a5
MaryShtayn <106402872+MaryShtayn@users.noreply.github.com>
2022-12-16 13:20:55 +0100

    task_2_done

--- src/js/task-02.js
+++ src/js/task-02.js
@@ -1,8 +1,19 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+
+const listEl = document.querySelector("ul#ingredients");
+const listItemEl = document.createElement("li");
+
+const ingredientsList = ingredients
+  .map((ingredient) => `<li class="item">${ingredient}</li>`)
+  .join("");
+
+listEl.innerHTML = ingredientsList;
+
+console.log(listEl);
