2e05042c680efece6e633ad9e7fe0b05de140f85
MaryShtayn <106402872+MaryShtayn@users.noreply.github.com>
2022-12-19 11:58:29 +0100

    task-06-fixed-done

--- src/js/task-06.js
+++ src/js/task-06.js
@@ -3,14 +3,15 @@ const input = document.querySelector('#validation-input');
 input.addEventListener('blur', onInputBlur);
 
 function onInputBlur(event) {
-    const inputLength = event.currentTarget.textContent.length;
-    const validLength = event.currentTarget.dataset.length;
- 
-    if ( Number(inputLength) === Number(validLength)) {
-        event.currentTarget.classList.add('.valid');
-          
-    }
-    
-    event.currentTarget.classList.add('.invalid');
-    
+  const inputLength = event.currentTarget.value.length;
+  const validLength = event.currentTarget.dataset.length;
+
+  if (Number(inputLength) === Number(validLength)) {
+    event.currentTarget.classList.add('valid');
+    event.currentTarget.classList.remove('invalid');
+    return;
+  }
+
+  event.currentTarget.classList.add('invalid');
+  event.currentTarget.classList.remove('valid');
 }
